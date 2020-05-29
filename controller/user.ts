2160472c777a31cc84df1342134a3ea5edd29f18
Kauê Murakami <kauetmurakami@gmail.com>
2020-05-29 16:01:16 -0300

    adicionando pesqiusa de usuarios e pesquisa com id como parametro

--- controller/user.ts
+++ controller/user.ts
@@ -1,31 +1,45 @@
 import { IUser } from '../model/user.ts'
 
 let users: Array<IUser> = [{
-    id: "1",
-    name: "Kauê Murakami",
-    email: "kaue@gmail.com",
-    create_at: new Date("2020-05-13"),
-    update_at: new Date("2020-05-13"),
-  }, {
-    id: "2",
-    name: "Luiza Costa",
-    email: "luiza@gmail.com",
-    create_at: new Date("2020-05-13"),
-    update_at: new Date("2020-05-13"),
-  }, {
-    id: "3",
-    name: "Ricardo da Silva",
-    email: "Ricardo@gmail.com",
-    create_at: new Date("2020-05-13"),
-    update_at: new Date("2020-05-13"),
-  }, {
-    id: "4",
-    name: "Maria Oliveira",
-    email: "maria@gmail.com",
-    create_at: new Date("2020-05-13"),
-    update_at: new Date("2020-05-13"),
-  }];
+  id: "1",
+  name: "Kauê Murakami",
+  email: "kaue@gmail.com",
+  create_at: new Date("2020-05-13"),
+  update_at: new Date("2020-05-13"),
+}, {
+  id: "2",
+  name: "Luiza Costa",
+  email: "luiza@gmail.com",
+  create_at: new Date("2020-05-13"),
+  update_at: new Date("2020-05-13"),
+}, {
+  id: "3",
+  name: "Ricardo da Silva",
+  email: "Ricardo@gmail.com",
+  create_at: new Date("2020-05-13"),
+  update_at: new Date("2020-05-13"),
+}, {
+  id: "4",
+  name: "Maria Oliveira",
+  email: "maria@gmail.com",
+  create_at: new Date("2020-05-13"),
+  update_at: new Date("2020-05-13"),
+}];
 
-  const getUsers = ({response} : {response: any }) => {
-      response.body = users
-  }
+const getUsers = ({ response }: { response: any }) => {
+  response.body = users
+}
+
+const getUser = ({ params, response }:
+  { params: { id: string }; response: any },) => {
+    const user : IUser | undefined = users.find((user) => user.id == params.id)
+    if(user){
+      response.status = 200
+      response.body = user
+    }else{
+      response.status = 404
+      response.body = { message: 'User not found.'}
+    }
+}
+
+export { getUsers, getUser };
--- router.ts
+++ router.ts
@@ -1,5 +1,5 @@
 import { Router } from "https://deno.land/x/oak/mod.ts"
-import { getUsers } from "./controller/user.ts"
+import { getUsers, getUser } from "./controller/user.ts"
 
 const router = new Router();
 
@@ -7,6 +7,7 @@ router.get("/", (context) => {
     context.response.body = "hello world"
 })
 
-router.get("users", getUsers)
+router.get("/users", getUsers)
 
+router.get("/user/:id", getUser)
 export default router;
